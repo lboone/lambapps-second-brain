60ddf62964a0eea9c4474225df360530229974b9
Lloyd Boone <lboone@progressive.com>
2024-06-19 20:58:53 -0400

    initial convex overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { Authenticated, Unauthenticated } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const createDocumen = useMutation(api.documents.createDocument);
+  const documents = useQuery(api.documents.getDocuments);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Unauthenticated>
@@ -10,7 +19,13 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
-        <div>Content</div>
+        <div>
+          <button onClick={() => createDocumen({ title: "hello world" })}>
+            Click Me
+          </button>
+
+          {documents?.map((doc) => <div key={doc._id}>{doc.title}</div>)}
+        </div>
       </Authenticated>
     </main>
   );
